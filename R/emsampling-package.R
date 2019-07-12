c12dda0ab0a14c6e940fbaf31c0207f70cb30e65
Gregory Jefferis <jefferis@gmail.com>
2019-07-12 22:29:14 +0100

    package docs

---
+++ R/emsampling-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+#'
+#'   It is worth looking at the documentation main class in the package,
+#'   \code{\link{samplingcurve}}, as well as the articles on the documentation website.
+#'
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
